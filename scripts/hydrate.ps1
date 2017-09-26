4b2a671cbc3274188fa9d2b6ca8c2df3a955ba2a
Mark DeLillo <mdelillo@pivotal.io>
2017-09-26 11:16:54 -0400

    Add scripts to run hydrator on Windows and *nix
    [#151008230]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

---
+++ scripts/hydrate.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+if ((go version) -NotLike "*go1.9*") {
+  echo "Must have go 1.9"
+  exit 1
+}
+
+$ROOTDIR=(Split-Path -Parent $PSScriptRoot)
+
+$env:GOPATH=$ROOTDIR
+
+cd $ROOTDIR
+$image_tag=(cat IMAGE_TAG)
+$image_name="cloudfoundry/windows2016fs"
+$output_dir="blobs/windows2016fs"
+mkdir -Force $output_dir
+
+go run src/hydrate/cmd/hydrate/main.go -image $image_name -outputDir $output_dir -tag $image_tag
