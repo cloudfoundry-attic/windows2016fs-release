d19b7d914d9d26f5bd441215587765fc722760a2
Mark DeLillo <mdelillo@pivotal.io>
2017-10-06 13:51:48 -0400

    Use code.cloudfoundry.org/windows2016fs submodule
    [#151682649]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- scripts/create-release.ps1
+++ scripts/create-release.ps1
@@ -15,12 +15,12 @@ $ROOTDIR=(Split-Path -Parent $PSScriptRoot)
 $env:GOPATH=$ROOTDIR
 
 Push-Location $ROOTDIR
-  $image_tag=(cat IMAGE_TAG)
+  $image_tag=(cat src/code.cloudfoundry.org/windows2016fs/IMAGE_TAG)
   $image_name="cloudfoundry/windows2016fs"
   $output_dir="blobs/windows2016fs"
   mkdir -Force $output_dir
 
-  go run src/oci-image/cmd/hydrate/main.go -image $image_name -outputDir $output_dir -tag $image_tag
+  go run src/code.cloudfoundry.org/windows2016fs/cmd/hydrate/main.go -image $image_name -outputDir $output_dir -tag $image_tag
 
   $release_version=(cat VERSION)
   bosh cr --version=$release_version
