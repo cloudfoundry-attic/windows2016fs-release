e6407dec14daea5fe72c8520a3a70ad10d64400c
Sam Smith <sesmith177@gmail.com>
2017-10-10 15:19:28 -0400

    Bump git timeout
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- src/create/main_test.go
+++ src/create/main_test.go
@@ -93,6 +93,6 @@ func checkoutDirectory(dir string) {
 		cmd.Dir = dir
 		gitSession, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
-		Eventually(gitSession, 5*time.Second).Should(gexec.Exit(0))
+		Eventually(gitSession, 20*time.Second).Should(gexec.Exit(0))
 	}
 }
