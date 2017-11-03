3f93024d2fc4749629b6b000e64d80203802f33d
Ehren Murdick <emurdick@pivotal.io>
2017-11-03 14:44:06 -0700

    qualify the package name
    Signed-off-by: Michelle He <mhe@pivotal.io>

--- src/create/main.go
+++ src/create/main.go
@@ -1,71 +1,13 @@
 package main
 
 import (
+	"create/createRelease"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-
-	"code.cloudfoundry.org/windows2016fs/hydrator"
-	"github.com/cloudfoundry/bosh-cli/cmd"
-	"github.com/cloudfoundry/bosh-cli/ui"
-	"github.com/cloudfoundry/bosh-utils/logger"
 )
-
-func CreateRelease(imageName, releaseDir, tarballPath, imageTagPath, versionDataPath, outputDir string) {
-	tagData, err := ioutil.ReadFile(imageTagPath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	imageTag := string(tagData)
-
-	h := hydrator.New(outputDir, imageName, imageTag)
-	if err := h.Run(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	versionData, err := ioutil.ReadFile(versionDataPath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	releaseVersion := cmd.VersionArg{}
-	if err := releaseVersion.UnmarshalFlag(string(versionData)); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	l := logger.NewLogger(logger.LevelInfo)
-	u := ui.NewConfUI(l)
-	defer u.Flush()
-	deps := cmd.NewBasicDeps(u, l)
-
-	createReleaseOpts := &cmd.CreateReleaseOpts{
-		Directory: cmd.DirOrCWDArg{Path: releaseDir},
-		Version:   releaseVersion,
-	}
-
-	if tarballPath != "" {
-		expanded, err := filepath.Abs(tarballPath)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		createReleaseOpts.Tarball = cmd.FileArg{FS: deps.FS, ExpandedPath: expanded}
-	}
-
-	createReleaseCommand := cmd.NewCmd(cmd.BoshOpts{}, createReleaseOpts, deps)
-	if err := createReleaseCommand.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
 
 func main() {
 	releaseDir, tarballPath, err := parseArgs()
@@ -82,7 +24,7 @@ func main() {
 
 	versionDataPath := filepath.Join(releaseDir, "VERSION")
 
-	CreateRelease(imageName, releaseDir, tarballPath, imageTagPath, versionDataPath, outputDir)
+	createRelease.CreateRelease(imageName, releaseDir, tarballPath, imageTagPath, versionDataPath, outputDir)
 }
 
 func parseArgs() (string, string, error) {
