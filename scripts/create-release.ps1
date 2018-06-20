2081e77652770383c8c13b8497474a04b2231d6f
Natalie Arellano <narellano@pivotal.io>
2018-06-20 14:18:51 -0400

    Update create-release scripts to always build create binaries on the fly
    We plan to not build these binaries as no one is using them.
    
    [#158500320]

--- scripts/create-release.ps1
+++ scripts/create-release.ps1
@@ -8,25 +8,10 @@ trap { $host.SetShouldExit(1) }
 $rootdir=(Split-Path -Parent $PSScriptRoot)
 $outfile = "$rootdir/bin/create.exe"
 
-if ($env:DEV_ENV -ne $null -and $env:DEV_ENV -ne "") {
-  $env:GOPATH=$rootdir
-  go build -o "$outfile" "$rootdir/src/create/main.go"
-  if ($LASTEXITCODE -ne 0) {
-    Exit $LASTEXITCODE
-  }
-} else {
-  $version=(cat "$rootdir/VERSION")
-  $sha=(cat "$rootdir/CREATE_BIN_SHA_WINDOWS")
-  $url="https://s3.amazonaws.com/windows2016fs/create-binaries/create-$version-windows-amd64.exe"
-
-  mkdir -Force "$rootdir/bin"
-  $wc = New-Object net.webclient
-  $wc.Downloadfile($url, $outfile)
-  $actualSha=(Get-FileHash -Path $outfile -Algorithm SHA256).Hash.ToLower()
-  if ("$actualSha" -ne "$sha") {
-    echo "$actualSha did not match expected sha256 $sha"
-    exit 1
-  }
+$env:GOPATH=$rootdir
+go build -o "$outfile" "$rootdir/src/create/main.go"
+if ($LASTEXITCODE -ne 0) {
+  Exit $LASTEXITCODE
 }
 
 if ($tarball -ne "") {
